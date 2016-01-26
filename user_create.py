6f66b85c9434bfab2a71d402aacd5f36328b1437
Jake Bradbeer <jakebradbeer@gmail.com>
2016-01-26 16:37:36 -0500

    Update user_create.py

--- user_create.py
+++ user_create.py
@@ -17,12 +17,3 @@ def new_user():
     except ():
         print ("ISSUE finding the database")
 
-
-def rec_user():
-    user_id = input("please enter your ID")
-
-    with open(atmdatabase.txt) as db:
-        for line in db:  # this is inefficient as it will run n times
-            if line.split(" ")[0] == user_id:
-                true_info = line
-                print true_info
