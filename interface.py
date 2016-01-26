80b6f35b29d34650f1552395db638ab7ce2a57b8
Jake Bradbeer <jakebradbeer@gmail.com>
2016-01-26 16:36:47 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,20 +1,8 @@
 import sys
 
 from user_create import new_user
-
-
-def rec_user():
-    user_id = input("please enter your ID")
-
-    script, atmdatabase = sys.argv
-
-    with open(atmdatabase.txt) as db:
-        for line in db:  # this is inefficient as it will run n times
-            if line.split(" ")[0] == user_id:
-                true_info = line
-                print true_info
-
-
+from bank_func import user_login
+ 
 responded = False
 while not responded:
     new_rec = raw_input("Are you a new user?")
